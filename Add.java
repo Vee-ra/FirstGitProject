76c0b142dde45f52a9fba8f4d23f4a7d53cdf691
Veera Sakthivel <vsvel.tsi@gmail.com>
2023-06-03 14:06:00 +0530

    Changed a to 15

--- Add.java
+++ Add.java
@@ -1,7 +1,7 @@
 public class Add {
 
     public static void main(String[] args) {
-        int a = 10, b = 2;
+        int a = 15, b = 2;
         int c = a + b;
         System.out.println(c);
     }
